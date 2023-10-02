ebdc7ddbde83b6ace9f989a59ccd4e7f57464607
Igor-bot-sudo <84375215+Igor-bot-sudo@users.noreply.github.com>
2023-10-02 11:14:37 +0300

    Added file Poison.py, into created class Poison.

---
+++ Poison.py
@@ -0,0 +1,6 @@
+class Poison:
+    # (имя, стоимость, какие предметы для создания необходимы)
+    def __init__(self, name, cost, subjects) -> None:
+      self.name = name
+      self.cost = cost
+      self.subjects = subjects
