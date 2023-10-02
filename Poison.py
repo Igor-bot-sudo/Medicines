a0bdfcbe30992537a002842cd7b8826aa5266a2e
Igor-bot-sudo <84375215+Igor-bot-sudo@users.noreply.github.com>
2023-10-02 11:43:50 +0300

    Added method check_description

--- Poison.py
+++ Poison.py
@@ -1,6 +1,19 @@
 class Poison:
-    # (имя, стоимость, какие предметы для создания необходимы)
-    def __init__(self, name, cost, subjects) -> None:
-      self.name = name
-      self.cost = cost
-      self.subjects = subjects
+    # def __init__(self, name, cost, subjects) -> None:
+    def __init__(self) -> None: 
+      self.medicals = {
+            'Medicinal decoctions': 'Подорожник-3 шт.',
+            'Fellow': 'Петрушка-1 шт., Подоржник-2 шт.',
+            'Robust': 'Черемуха-3шт., Петрушка-2шт., Подорожник-1 шт.'
+        }
+
+    #   self.name = name
+    #   self.cost = cost
+    #   self.subjects = subjects
+
+    def check_description(self, name: str) -> str | None:
+       if name in self.medicals.keys():
+          return self.medicals[name]
+       else:
+          return None
+          
